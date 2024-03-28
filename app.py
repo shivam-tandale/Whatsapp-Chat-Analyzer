79a2b9809a50ce24a865689e208e36647127c84b
shivam-tandale <95087339+shivam-tandale@users.noreply.github.com>
2024-03-28 21:04:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ if uploaded_file is not None:
     # user_list.remove('group_notification')
     if 'group_notification' in user_list:
     user_list.remove('group_notification')
-else:
+    else:
     print("'group_notification' not found in user_list")
     user_list.sort()
     user_list.insert(0,"Overall")
