319f3f41ce9e3d707c908b1de622f2605c815641
shivam-tandale <95087339+shivam-tandale@users.noreply.github.com>
2024-03-28 21:03:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,11 @@ if uploaded_file is not None:
 
     # fetch unique users
     user_list = df['user'].unique().tolist()
+    # user_list.remove('group_notification')
+    if 'group_notification' in user_list:
     user_list.remove('group_notification')
+else:
+    print("'group_notification' not found in user_list")
     user_list.sort()
     user_list.insert(0,"Overall")
 
